105d76d4b9fb599f69452c9251b633929ae75b98
sudo-lucifer <krittin.niu@student.mahidol.edu>
2023-01-14 12:33:12 +0700

    Test

--- vending_machine_backend/settings.py
+++ vending_machine_backend/settings.py
@@ -66,9 +66,7 @@ MIDDLEWARE = [
 REST_FRAMEWORK = {
     # Use Django's standard `django.contrib.auth` permissions,
     # or allow read-only access for unauthenticated users.
-    'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
-    ]
+    'DEFAULT_PERMISSION_CLASSES': ['rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly']
 }
 
 ROOT_URLCONF = 'vending_machine_backend.urls'
