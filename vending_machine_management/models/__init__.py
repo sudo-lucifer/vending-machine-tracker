011e69bfb034a03565ce859f56ce6295966a20a5
sudo-lucifer <krittin.niu@student.mahidol.edu>
2023-01-14 14:44:14 +0700

    [Model] Add stock

--- vending_machine_management/models/__init__.py
+++ vending_machine_management/models/__init__.py
@@ -1,4 +1,5 @@
 from vending_machine_management.models.product import Product
+from vending_machine_management.models.stock import Stock
 from vending_machine_management.models.vending_machine import VendingMachine
 
-__all__ = ["VendingMachine", "Product"]
+__all__ = ["VendingMachine", "Product", "Stock"]
---
+++ vending_machine_management/models/stock.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+from vending_machine_management.models.base_model import BaseModel
+
+# Create your models here.
+
+
+class Stock(BaseModel):
+    id = models.BigAutoField(primary_key=True)
+    vending_machine = models.ForeignKey(
+        to="vending_machine_management.VendingMachine", on_delete=models.CASCADE, null=False
+    )
+    product = models.ForeignKey(to="vending_machine_management.Product", on_delete=models.CASCADE, null=False)
+    amount = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
