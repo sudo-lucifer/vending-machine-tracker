95ec27f04e1a9d4af231ff7cbdb445f2b6366fd1
sudo-lucifer <krittin.niu@student.mahidol.edu>
2023-01-24 00:44:04 +0700

    [Machine View Test] Added retrieve and edit machine API tests

---
+++ vending_machine_management/tests/views/test_machine_view/test_edit_machine_view.py
@@ -0,0 +1,48 @@
+import random
+import string
+from typing import Dict
+
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
+
+from vending_machine_management.tests.model_instances.machine_model_instance import machine_instance
+
+
+class TestEditMachineView(APITestCase):
+    @classmethod
+    def setUpTestData(cls):
+        machine_1 = machine_instance.make()
+        cls.machine_1 = machine_1
+        cls.machine_2 = machine_instance.make()
+        cls.url = reverse("vending_machine:retrieve-update-destroy", kwargs={"id": machine_1.id})
+
+    def test_edit_single_machine_invalid_input(self):
+        response = self.client.put(self.url)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+        request_body: Dict[str, str] = {"name": "Test 1"}
+        response = self.client.put(self.url, data=request_body)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+        request_body: Dict[str, str] = {"location": "Location 1"}
+        response = self.client.put(self.url, data=request_body)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_add_machine_correct_input(self):
+        new_machine_name = ''.join(random.choices(string.ascii_uppercase + string.digits, k=100))
+        request_body: Dict[str, str] = {"name": new_machine_name, "location": self.machine_1.location}
+
+        response = self.client.put(self.url, data=request_body)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['id'], self.machine_1.id)
+        self.assertEqual(response.data['name'], request_body['name'])
+        self.assertEqual(response.data['location'], request_body['location'])
+
+    def test_add_machine_duplicate_name(self):
+        request_body: Dict[str, str] = {"name": self.machine_2.name, "location": self.machine_1.location}
+
+        response = self.client.put(self.url, data=request_body)
+
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
--- vending_machine_management/tests/views/test_machine_view/test_retrieve_machine_view.py
+++ vending_machine_management/tests/views/test_machine_view/test_retrieve_machine_view.py
@@ -1,5 +1,5 @@
 import json
-from typing import Dict, List
+from typing import Dict
 
 from django.urls import reverse
 from rest_framework import status
@@ -9,20 +9,16 @@ from vending_machine_management.serializers.machine_serializer import MachineSer
 from vending_machine_management.tests.model_instances.machine_model_instance import machine_instance
 
 
-class TestListMachineView(APITestCase):
+class TestRetrieveMachineView(APITestCase):
     @classmethod
     def setUpTestData(cls):
         cls.machine_1 = machine_instance.make()
-        cls.machine_2 = machine_instance.make()
-        cls.url = reverse("vending_machine:list-create")
 
-    def test_list_machine(self):
-        response = self.client.get(self.url)
+    def test_retrieve_single_machine(self):
+        url = reverse("vending_machine:retrieve-update-destroy", kwargs={"id": self.machine_1.id})
+        response = self.client.get(url)
 
-        expected_result: List[Dict[str, str]] = [
-            MachineSerializer(self.machine_1).data,
-            MachineSerializer(self.machine_2).data,
-        ]
-        response_data: List[Dict[str, str]] = json.loads(json.dumps(response.data))
+        expected_result: Dict[str, str] = MachineSerializer(self.machine_1).data
+        response_data: Dict[str, str] = json.loads(json.dumps(response.data))
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response_data, expected_result)
