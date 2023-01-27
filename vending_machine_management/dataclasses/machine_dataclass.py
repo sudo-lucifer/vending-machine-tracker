fee6fbdb11b47d1ab9334afe83d42ce83c541db4
sudo-lucifer <krittin.niu@student.mahidol.edu>
2023-01-27 19:58:34 +0700

    Fix lint

--- vending_machine_management/dataclasses/machine_dataclass.py
+++ vending_machine_management/dataclasses/machine_dataclass.py
@@ -8,10 +8,17 @@ from vending_machine_management.models.machine import Machine
 @dataclass_json
 @dataclass
 class MachineDataclass:
+    """Dataclass for machine model."""
+
     id: int
     name: str
     location: str
 
     @classmethod
-    def from_model(cls, machine_instance: Machine):
+    def from_model(cls, machine_instance: Machine) -> 'MachineDataclass':
+        """Categorize raw machine model to dataclass.
+
+        :param machine_instance: query instance from machine model
+        :return: dataclass representation of machine query instance
+        """
         return cls(id=machine_instance.id, name=machine_instance.name, location=machine_instance.location)
